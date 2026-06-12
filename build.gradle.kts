0902b257c7b678fae2f5dd54a83fc12f1bf5e00c
Derek Burned <danillolka2@gmail.com>
2026-06-12 17:00:05 +0200

    chore: clean up and align dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.kapt) apply false
     alias(libs.plugins.objectbox) apply false
 }
